aa969b8f9a1cd1b9d2784d40b95830a4d2f72d4f
kushal-10 <kushalkoshti11@gmail.com>
2025-03-25 20:14:05 +0100

    add categories, instances, ade_map.assign_type

--- src/mapworld/ade_maps.py
+++ src/mapworld/ade_maps.py
@@ -1,8 +1,11 @@
 from src.mapworld.maps import BaseMap
+import os
+import json
+import numpy as np
 
 class ADEMap(BaseMap):
 
-    def __init__(self, m: int = 3, n: int = 3, n_rooms: int = 9, target_distr: list[int] = [1]):
+    def __init__(self, m: int = 3, n: int = 3, n_rooms: int = 9):
         """
         Set up a base 2-D graph whose nodes are based on ADE20k dataset.
         
@@ -10,33 +13,103 @@ class ADEMap(BaseMap):
             m: Number of rows in the graph.
             n: Number of columns in the graph
             n_rooms: Required number of rooms. Should be less than n*m
-            target_distr: List of integers to control ambiguity. Example: [3,2] means - at
-            least two types of potential target categories, and that
-            the first will occur three times, and the second twice.
 
         Raises:
             ValueError: If any value is unset
             AssertionError: If `n_rooms` > `n*m`
         """
         super().__init__(m, n, n_rooms)
-        self.distribution = target_distr
 
     
-    def assign_types(self, G):
+    def assign_types(self, G, json_path: str = os.path.join("src", "mapworld", "categories.json"), ambiguity: list[int] = [1]):
         """
         Assign room categories and images to the nodes in the generated graph.
+        Example G.nodes[room] = {
+            'base_type': 'indoor',
+            'type': 'k/kitchen',
+            'target': True
+        }
 
         Args:
             G: Generated graph. (via BaseMap.create_acyclic_graph()/BaseMap.create_cyclic_graph())
+            json_path: Path to a json file containing "targets", "outdoors" and "distractors" categories
+            ambiguity: List of integers to control ambiguity. Example: [3,2] means - at
+            least two types of potential target categories, and that
+            the first will occur three times, and the second twice. Only applies to indoor rooms
+        
+        Raises:
+            ValueError: if possible indoor rooms (rooms with more than 1 neighbor) < sum of ambiguity
         """
-        pass
+        
+        outdoor_rooms = []
+        indoor_rooms = []
+        for room in G.nodes():
+            if G.degree(room) == 1:
+                outdoor_rooms.append(room)
+            elif G.degree(room) > 1:
+                indoor_rooms.append(room)
+            else:
+                raise ValueError(f"Check Graph Generation!! Found a node with no neighbors!")
 
+        if len(indoor_rooms) < sum(ambiguity):
+            raise ValueError(
+                f"Ambiguity passed is {ambiguity}, But number of indoor rooms in the generated graph is {len(indoor_rooms)}"
+                f"Try decreasing ambiguity such that sum of ambiguity is <= {len(indoor_rooms)}"
+                f"If ambiguity is strictly required and possible with the initialized Map, try generating another random graph.")
+        
+        with open(json_path, 'r') as f:
+            categories = json.load(f)
+
+        outdoor_room_assigned = []
+        for room in outdoor_rooms:
+            # Keep ambiguity 1 for outdoor rooms
+            # TODO : Add ambiguity for outdoor rooms as well, thinking specifically for EscapeRoom Game
+            random_outdoor_room = np.random.choice(categories["outdoors"])
+            while random_outdoor_room in outdoor_room_assigned:
+                random_outdoor_room = np.random.choice(categories["outdoors"])
+
+            outdoor_room_assigned.append(random_outdoor_room)
+
+            G.nodes[room]['base_type'] = "outdoor"
+            G.nodes[room]['type'] = random_outdoor_room
+            G.nodes[room]['target'] = False     
+    
+        # TODO: Pass 'type' as argument as well, to pre set certain type of rooms
+        # atleast 2 home_office and 2 bedrooms, for example
+        
+        indoor_rooms_assigned = []
+        room_type_assigned = []
+        for room_type in ambiguity:
+            # TODO: Add check to see length of ambiguity <= len(categories["targets"])
+            random_room_type = np.random.choice(categories["targets"])
+            while random_room_type in room_type_assigned:
+                random_room_type = np.random.choice(categories["targets"])
+            room_type_assigned.append(random_room_type)
+
+            for r in range(room_type):
+                room = indoor_rooms[np.random.randint(len(indoor_rooms))]
+                while room in indoor_rooms_assigned:
+                    room = indoor_rooms[np.random.randint(len(indoor_rooms))]
+                indoor_rooms_assigned.append(room)
+
+                G.nodes[room]['base_type'] = "indoor"
+                G.nodes[room]['type'] = random_room_type
+                G.nodes[room]['target'] = True
+
+        # Remaining nodes - as distractors
+        print(set(indoor_rooms) - set(indoor_rooms_assigned))    
+
+        return G
 
 if __name__ == '__main__':
 
-    ademap = ADEMap(3, 3, 5, [1,1])
+    ademap = ADEMap(3, 3, 5)
     G = ademap.create_cyclic_graph(1)
-    ademap.plot_graph(G)
+    G = ademap.assign_types(G, ambiguity=[2,2])
+
+    nodes = G.nodes()
+    for n in nodes:
+        print(G.nodes[n])
 
 
 
